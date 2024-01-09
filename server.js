68ef6a5833b781661aa830f704070fe7378386e3
Prince Kumar <princekumar776595@outlook.com>
2024-01-09 18:58:36 +0530

    updated serverjs file

--- server.js
+++ server.js
@@ -6,16 +6,28 @@ import morgan from "morgan";
 
 const app = Express();
 
+//DOTENV CONFIG
+
+dotenv.config();
+
 // Middlewares
 
 app.use(cors());
 app.use(Express.json());
 app.use(morgan("dev"));
 
-app.listen(3000);
-
 app.get("/", (req, res) => {
   return res
     .status(200)
     .send("<h1>Welcome to FOod Server APP API BASE PROJECT </h1>");
+});
+
+//PORT
+
+const PORT = process.env.PORT || 8080;
+
+//server listening
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`.yellow.bold);
 });
