5d07cd094fd15071f7f863bc100e216263a18318
Prince Kumar <princekumar776595@outlook.com>
2024-01-09 18:48:09 +0530

    updated serverjs file

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 import Express from "express";
+import colors from "colors";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
 
 const app = Express();
+
+// Middlewares
+
+app.use(cors());
+app.use(Express.json());
+app.use(morgan("dev"));
 
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  return res.status(200).send("Hello World");
+  return res
+    .status(200)
+    .send("<h1>Welcome to FOod Server APP API BASE PROJECT </h1>");
 });
