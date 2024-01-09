60035b2a15edf8707f9b3752f4185bf3a09b9057
Prince Kumar <princekumar776595@outlook.com>
2024-01-09 18:16:14 +0530

    First commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = Express();
 app.listen(3000);
 
 app.get("/" ,(req,res) => {
-    return res.status(200).send("<h1>Welcome Food Server App</h1>");
+    return res.status(200).send("Hello World"); 
 
 
 
