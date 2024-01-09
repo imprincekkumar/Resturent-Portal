8d200462705d5a4789a6e212aab5d79cb78deb27
Prince Kumar <princekumar776595@outlook.com>
2024-01-09 18:32:07 +0530

    updated serverjs file

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-const Express = require("express");
+import Express from "express";
 
 const app = Express();
 
 app.listen(3000);
 
-app.get("/" ,(req,res) => {
-    return res.status(200).send("Hello World"); 
-
-
-
+app.get("/", (req, res) => {
+  return res.status(200).send("Hello World");
 });
